c4e2f1d40d92860558ca6ed879c71eb15905b16a
Taiye Salami <taiye41@gmail.com>
2020-03-03 23:13:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -14,3 +14,27 @@ new_hash = {}
   return new_hash
 
 end
+
+
+def personal_attributes(data)
+  unique_name = []
+  hash = {}
+  data.each do |attribute, value|
+    value.each do |type, names|
+      names.each do |ele|
+        if !ele 
+          unique_name << ele
+        end
+        unique_name.each do |first_name|
+          if first_name == ele
+            hash[:attribute] = type
+          end
+        end 
+      end
+    end
+  end
+
+  return hash
+       
+end
+
