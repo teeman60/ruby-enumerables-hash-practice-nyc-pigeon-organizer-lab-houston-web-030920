aae53f2aa1c5d5b84fde923677e41bca409c69c5
Taiye Salami <taiye41@gmail.com>
2020-03-03 21:49:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+new_hash = {}
+  data.each do |attribute, value|
+    value.each do |type, names|
+      names.each do |ele|
+        if !new_hash[ele]
+        new_hash[ele] = somthn
+        end
+      end
+    end
+  end
+
+  return new_hash
+
 end
